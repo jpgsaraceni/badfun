6f297e7b2166dd832268e2d7d73dbd702a6f0974
jpgsaraceni <jpgomes@id.uff.br>
2022-02-01 14:29:21 -0300

    test: Add delete controller test cases

--- app/gateways/api/http/controllers/clients/DeleteEntry.test.js
+++ app/gateways/api/http/controllers/clients/DeleteEntry.test.js
@@ -31,4 +31,74 @@ describe('#controllers.DeleteEntry.js', function() {
         expect(sendStatusSpy.calledOnce).to.be.true
         expect(sendStatusSpy.firstCall.args[0]).to.be.equal(200)
     })
+
+    it('should respond status 400 when request missing id', async function() {
+        const req = {
+            params: {}
+        }
+
+        const sendStatusSpy = Sinon.spy()
+
+        const res = {
+            sendStatus: sendStatusSpy
+        }
+
+        const clientRepositoryMock = new ClientRepositoryMock
+        const deleteEntry = DeleteEntry(clientRepositoryMock)
+        
+        await deleteEntry(req, res)
+
+        expect(sendStatusSpy.calledOnce).to.be.true
+        expect(sendStatusSpy.firstCall.args[0]).to.be.equal(400)
+    })
+
+    it('should respond status 404 when id does not exist', async function() {
+        const testId = new mongoose.Types.ObjectId().toString()
+        
+        const req = {
+            params: {
+                id: testId
+            }
+        }
+
+        const sendStatusSpy = Sinon.spy()
+
+        const res = {
+            sendStatus: sendStatusSpy
+        }
+
+        const clientRepositoryMock = new ClientRepositoryMock
+        Sinon.stub(clientRepositoryMock, "DeleteEntry").returns(null)
+        const deleteEntry = DeleteEntry(clientRepositoryMock)
+        
+        await deleteEntry(req, res)
+
+        expect(sendStatusSpy.calledOnce).to.be.true
+        expect(sendStatusSpy.firstCall.args[0]).to.be.equal(404)
+    })
+
+    it('should respond status 500 due to repository error', async function() {
+        const testId = new mongoose.Types.ObjectId().toString()
+        
+        const req = {
+            params: {
+                id: testId
+            }
+        }
+
+        const sendStatusSpy = Sinon.spy()
+
+        const res = {
+            sendStatus: sendStatusSpy
+        }
+
+        const clientRepositoryMock = new ClientRepositoryMock
+        Sinon.stub(clientRepositoryMock, "DeleteEntry").rejects()
+        const deleteEntry = DeleteEntry(clientRepositoryMock)
+        
+        await deleteEntry(req, res)
+
+        expect(sendStatusSpy.calledOnce).to.be.true
+        expect(sendStatusSpy.firstCall.args[0]).to.be.equal(500)
+    })
 })
